52c1dd54a4254817413b9ab8c8eed0ce4bce815d
Raza Robson <rz.robsn@gmail.com>
2012-10-03 20:20:59 -0400

    added BoardListener interface methods.

--- jolies-dames/src/main/java/com/jolies/dames/utilities/glviews/BoardListener.java
+++ jolies-dames/src/main/java/com/jolies/dames/utilities/glviews/BoardListener.java
@@ -2,5 +2,17 @@ package com.jolies.dames.utilities.glviews;
 
 public interface BoardListener
 {
-    
+    /**
+     * Executes when the user(s) has selected 
+     * the slot at position (x, y) of the board.
+     * 
+     * @param x
+     * @param y
+     */
+    public void onSlotSelected(int x, int y);
+
+    /**
+     * Executes when the user(s) requests a new game.
+     */
+    public void onNewGameRequested();
 }
