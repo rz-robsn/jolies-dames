f30b1f50d3370e5d0b2807ee5bcc7431e5f2d2a0
Raza Robson <rz.robsn@gmail.com>
2012-11-10 10:22:45 -0500

    refactored.

--- jolies-dames/src/main/java/com/jolies/dames/utilities/RendererGameView.java
+++ jolies-dames/src/main/java/com/jolies/dames/utilities/RendererGameView.java
@@ -86,31 +86,8 @@ public class RendererGameView implements GLSurfaceView.Renderer
 		// view matrix. In OpenGL 2, we can keep track of these matrices separately if we choose.
 		Matrix.setLookAtM(mViewMatrix, 0, eyeX, eyeY, eyeZ, lookX, lookY, lookZ, upX, upY, upZ);
 
-		final String vertexShader =
-			"uniform mat4 u_MVPMatrix;      \n"		// A constant representing the combined model/view/projection matrix.
-			
-		  + "attribute vec4 a_Position;     \n"		// Per-vertex position information we will pass in.
-		  + "attribute vec4 a_Color;        \n"		// Per-vertex color information we will pass in.			  
-		  
-		  + "varying vec4 v_Color;          \n"		// This will be passed into the fragment shader.
-		  
-		  + "void main()                    \n"		// The entry point for our vertex shader.
-		  + "{                              \n"
-		  + "   v_Color = a_Color;          \n"		// Pass the color through to the fragment shader. 
-		  											// It will be interpolated across the triangle.
-		  + "   gl_Position = u_MVPMatrix   \n" 	// gl_Position is a special variable used to store the final position.
-		  + "               * a_Position;   \n"     // Multiply the vertex by the matrix to get the final point in 			                                            			 
-		  + "}                              \n";    // normalized screen coordinates.
-		
-		final String fragmentShader =
-			"precision mediump float;       \n"		// Set the default precision to medium. We don't need as high of a 
-													// precision in the fragment shader.				
-		  + "varying vec4 v_Color;          \n"		// This is the color from the vertex shader interpolated across the 
-		  											// triangle per fragment.			  
-		  + "void main()                    \n"		// The entry point for our fragment shader.
-		  + "{                              \n"
-		  + "   gl_FragColor = v_Color;     \n"		// Pass the color directly through the pipeline.		  
-		  + "}                              \n";												
+		final String vertexShader = HelperRendererGameView.getVertexShader();		
+		final String fragmentShader = HelperRendererGameView.getFragmentShader();												
 		
 		// Load in the vertex shader.
 		int vertexShaderHandle = HelperRendererGameView.compileShader(GLES20.GL_VERTEX_SHADER, vertexShader);
